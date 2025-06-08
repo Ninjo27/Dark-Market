b25d1657192be261d601e268b04980c6c5e62b4e
Ninjo27 <taking2107@gmail.com>
2025-06-08 11:15:03 +0700

    Update sever.js

--- sever.js
+++ sever.js
@@ -12,3 +12,10 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
     .catch(err => console.log(err));
 
 app.listen(5000, () => console.log("Server running on port 5000"));
+const authRoutes = require("./routes/auth");
+const coinRoutes = require("./routes/coins");
+const shopRoutes = require("./routes/shop");
+
+app.use("/api/auth", authRoutes);
+app.use("/api/coins", coinRoutes);
+app.use("/api/shop", shopRoutes);
